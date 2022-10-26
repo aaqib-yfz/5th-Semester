8423edeaf7d5d40666af600b1e96c0b2286b817d
aaqib-yfz <aaqibshaheer2001@gmail.com>
2022-10-26 09:17:45 -0400

    first fed

--- src/Threads/twenty_fifth_oct_exmp/Counter.java
+++ src/Threads/twenty_fifth_oct_exmp/Counter.java
@@ -2,12 +2,12 @@ package Threads.twenty_fifth_oct_exmp;
 
 public class Counter {
     private int count =0;
-    public void increment(){
+    public  synchronized void increment(){
       for (int i =0; i<=100000; i++) {
           count=count+1;
       }
       }
-    public void decrement(){
+    public synchronized void decrement(){
        for(int i = 0; i<= 100000; i++) {
            count--;
        }
